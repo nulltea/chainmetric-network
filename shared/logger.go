e822bc73f2e47f4e47194013de2bae5c63c7d5f0
Timothy <timauthx@gmail.com>
2021-05-26 04:57:48 +0300

    Improvement: add viper for env vars usage

--- shared/logger.go
+++ shared/logger.go
@@ -4,29 +4,36 @@ import (
 	"os"
 
 	"github.com/op/go-logging"
+	"github.com/spf13/viper"
 )
 
-var (
-	// Logger is the shared logger instance.
-	Logger = logging.MustGetLogger(ChaincodeName)
-)
+// Logger is an instance of the shared logger tool.
+var Logger *logging.Logger
 
 const (
-	format = "%{color}%{time:2006.01.02 15:04:05} %{id:04x} %{level:.4s}%{color:reset} [%{module}] %{color:bold}%{shortfunc}%{color:reset} -> %{message}"
+	format = "%{color}%{time:2006.01.02 15:04:05} " +
+		"%{id:04x} %{level:.4s}%{color:reset} " +
+		"[%{module}] %{color:bold}%{shortfunc}%{color:reset} -> %{message}"
 )
 
 func initLogger() {
+	var (
+		envLevel = viper.GetString("logging")
+		chaincodeName = viper.GetString("name")
+	)
+
+	Logger = logging.MustGetLogger(chaincodeName)
 
 	backend := logging.NewBackendFormatter(
 		logging.NewLogBackend(os.Stderr, "", 0),
 		logging.MustStringFormatter(format),
 	)
 
-	level, err := logging.LogLevel(LogLevel); if err != nil {
-		Logger.Warningf("failed to parse '%s' as a log level", LogLevel)
+	level, err := logging.LogLevel(envLevel); if err != nil {
+		Logger.Warningf("failed to parse '%s' as a log level", envLevel)
 		level = logging.INFO
 	}
 
 	logging.SetBackend(backend)
-	logging.SetLevel(level, ChaincodeName)
+	logging.SetLevel(level, chaincodeName)
 }
