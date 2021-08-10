d7648498375e4315fb7247b83a46b08be35532a7
Timothy Yalugin <timauthx@gmail.com>
2021-08-10 04:59:37 +0300

    Add user-related models

--- model/user/registration.go
+++ model/user/registration.go
@@ -1,1 +1,7 @@
 package user
+
+type RegistrationRequest struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+	Email string `json:"email"`
+}
--- model/user/user.go
+++ model/user/user.go
@@ -1,11 +1,25 @@
-package users
+package user
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+// User defines structure of user model.
+//
+// swagger:model
 type User struct {
-	Firstname string `json:"firstname" bson:"firstname"`
+	ID           string `json:"id" bson:"id"`
+	EnrollmentID string `json:"enrollment_id" bson:"enrollment_id"`
+	Firstname    string `json:"firstname" bson:"firstname"`
 	Lastname string `json:"lastname" bson:"lastname"`
 	Email string `json:"email" bson:"email"`
 	Role  string `json:"role" bson:"role"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	Confirmed bool `json:"confirmed" bson:"confirmed"`
+}
+
+// IdentityName forms the unique name of user's identity.
+func (u *User) IdentityName() string {
+	return fmt.Sprintf("user.%s_%s", u.Firstname, u.Lastname)
 }
