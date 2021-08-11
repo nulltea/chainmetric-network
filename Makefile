e94d16662b3e0f3b4dd236eac598079fe27b9024
Timothy Yalugin <timauthx@gmail.com>
2021-08-11 03:43:46 +0300

    Move `presenter` package to API layer

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 swagger:
-	swag init -g ./src/users/main.go -o ./src/users/docs
+	swag init -g ./src/users/doc.go -o ./src/users/docs
--- src/users/main.go
+++ src/users/main.go
@@ -13,19 +13,6 @@ func init() {
 	identity.Init()
 }
 
-// @title Chainmetric Identity API
-// @version 1.0
-// @description Chainmetric Identity API users to authenticate and save preferences.
-
-// @contact.name Timothy Yalugin
-// @contact.url https://github.com/timoth-y
-// @contact.email timauthx@gmail.com
-
-// @license.name Apache 2.0
-// @license.url https://raw.githubusercontent.com/timoth-y/chainmetric-contracts/main/LICENSE
-
-// @host identity.chainmetric.network
-// @BasePath /
 func main() {
 	engine := gin.Default()
 	api.Setup(engine)
