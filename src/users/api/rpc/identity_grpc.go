15dcf4882a4b1126e5fadaee2779ce28aec691b8
Timothy Yalugin <timauthx@gmail.com>
2021-08-16 04:43:59 +0300

    gRPC WIP

--- src/users/api/rpc/identity_grpc.go
+++ src/users/api/rpc/identity_grpc.go
@@ -5,12 +5,20 @@ import (
 
 	"github.com/timoth-y/chainmetric-contracts/src/users/api/presenter"
 	"github.com/timoth-y/chainmetric-contracts/src/users/usecase/identity"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type identityService struct{}
 
+
+func IdentityServiceGRPC(server *grpc.Server) {
+	RegisterIdentityServiceServer(server, &identityService{})
+}
+
+// Register implements IdentityServiceClient gRPC service.
 func (identityService) Register(
 	ctx context.Context,
 	req *presenter.RegistrationRequest,
@@ -31,16 +39,18 @@ func (identityService) Register(
 	return presenter.NewUserProto(*user), nil
 }
 
-func (identityService) Enroll(ctx context.Context, req *presenter.EnrollmentRequest) error {
+// Enroll implements IdentityServiceClient gRPC service.
+func (identityService) Enroll(ctx context.Context, req *presenter.EnrollmentRequest) (*emptypb.Empty, error) {
 	if err := req.Validate(); err != nil {
-		return status.Error(codes.InvalidArgument, err.Error())
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	if err := identity.Enroll(req.UserID,
 		identity.WithRole(req.Role),
+		identity.WithExpirationPb(req.ExpireAt),
 	); err != nil {
-		return status.Error(codes.Internal, err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
-	return nil
+	return nil, nil
 }
